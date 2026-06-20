af427d6b4695b607a3d35314da676f83f45fb29a
Phonicmas <jkskovlund@gmail.com>
2026-06-20 19:50:02 +0200

    fix explosion reset on load

--- 1.6/Source/Abhuman40k/Buildings/Building_HerdstoneEnemy.cs
+++ 1.6/Source/Abhuman40k/Buildings/Building_HerdstoneEnemy.cs
@@ -1,10 +1,34 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using Verse;
 
 namespace Abhuman40k;
 
 public class Building_HerdstoneEnemy : Building
 {
+
+    private List<Pawn> newGroupToAttack = [];
+
+    private int waveNumber = 1;
+
+    private SimpleCurve minotaurPercentage =
+    [
+        new CurvePoint(1, 0f),
+        new CurvePoint(3, 0.2f),
+        new CurvePoint(5, 0.4f),
+        new CurvePoint(8, 0.6f),
+        new CurvePoint(10, 1f)
+    ];
+    
+    private SimpleCurve groupPointStrength =
+    [
+        new CurvePoint(1, 1f),
+        new CurvePoint(3, 1.2f),
+        new CurvePoint(5, 1.4f),
+        new CurvePoint(8, 1.6f),
+        new CurvePoint(10, 2f)
+    ];
+    
     protected override void Tick()
     {
         base.Tick();
